394383f7b5373199e8d7f60e2fbd2dce4ce9e306
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-19 02:15:37 +0900

    commit

--- RealmTest/ContentView.swift
+++ RealmTest/ContentView.swift
@@ -10,13 +10,15 @@ import RealmSwift
 
 struct ContentView: View {
     var allUser: Results<UserTable>?
+    var user:UserTable = UserTable(value: ["ID" : 1 ,
+                                           "userName" : "user2"])
+    @State var hoge: String = "a"
+    var realm: Realm
+
     
     init(){
         //デフォルトRealmの取得
-        let realm = try! Realm()
-        //追加するデータの生成
-        let user:UserTable = UserTable(value: ["ID" : 2 ,
-                                               "userName" : "user2"])
+        realm = try! Realm()
         
 
         try! realm.write{
@@ -29,16 +31,27 @@ struct ContentView: View {
         //データの取得
         allUser = realm.objects(UserTable.self)
     }
-
+    
     var body: some View {
-        VStack(){
-            Text("登録されたユーザ")
-            HStack {
-                Text(allUser![0].ID.description)
-                Text(allUser![0].userName)
+        VStack {
+            Button(action: {
+                // ボタン効いてるか適当に確認
+                hoge = Int.random(in: 1..<10).description
+                
+            }){
+                Text("Add")
+            }
+            VStack(){
+                Text("登録されたユーザ")
+                VStack {
+                    ForEach(0..<allUser!.count, id: \.self) { num in
+                        Text(allUser![num].ID.description)
+                        Text(allUser![num].userName)
+                        Text(hoge)
+                    }
+                }
             }
         }
-
     }
 }
 
