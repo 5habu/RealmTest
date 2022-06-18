6daccd41f368da698dce077310ad08d1c7393445
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-19 00:34:42 +0900

    Initial Commit

---
+++ RealmTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmTest
+//
+//  Created by 5habu on 2022/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmTest/RealmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTestApp.swift
+//  RealmTest
+//
+//  Created by 5habu on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct RealmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
